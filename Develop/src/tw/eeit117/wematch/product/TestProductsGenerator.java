1b31573d2e8894ea49a68fda896d8c049c92c0aa
yisheng112358 <chenyisheng112358@gmail.com>
2020-09-09 20:23:47 +0800

    update the java bean

--- Develop/src/tw/eeit117/wematch/product/TestProductsGenerator.java
+++ Develop/src/tw/eeit117/wematch/product/TestProductsGenerator.java
@@ -13,6 +13,8 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+import tw.eeit117.wematch.product.model.ProductBean;
+
 public class TestProductsGenerator {
 
 	public static void main(String[] args) throws IOException {
@@ -21,74 +23,74 @@ public class TestProductsGenerator {
 		Session session = factory.openSession();
 		session.beginTransaction();
 
-		Product product1 = new Product("Massager", "3D massager 鑽石按摩儀", 299, 1, "微雕按摩儀/體雕棒(按摩儀)",
+		ProductBean product1 = new ProductBean("Massager", "3D massager 鑽石按摩儀", 299, 1, "微雕按摩儀/體雕棒(按摩儀)",
 				imageToByteArray("resource\\massager\\massager1_t.jpg"),
 				imageToByteArray("resource\\massager\\massager1_d.jpg"));
 		session.save(product1);
 
-		Product product2 = new Product("Massager", "Thera-Band Roller Massager", 1000, 8, "標準版 滾筒按摩器(免運費)",
+		ProductBean product2 = new ProductBean("Massager", "Thera-Band Roller Massager", 1000, 8, "標準版 滾筒按摩器(免運費)",
 				imageToByteArray("resource\\massager\\massager2_t.jpg"),
 				imageToByteArray("resource\\massager\\massager2_d.jpg"));
 		session.save(product2);
 
-		Product product3 = new Product("Massager", "Fascia Gun Storage Massage", 584, 2,
+		ProductBean product3 = new ProductBean("Massager", "Fascia Gun Storage Massage", 584, 2,
 				"Gun Base Massage Gun Stand Fasc", imageToByteArray("resource\\massager\\massager3_t.jpg"),
 				imageToByteArray("resource\\massager\\massager3_d.jpg"));
 		session.save(product3);
 
-		Product product4 = new Product("Massager", " Melon low-frequency electric therapy", 2489, 1,
+		ProductBean product4 = new ProductBean("Massager", " Melon low-frequency electric therapy", 2489, 1,
 				"Massage machine white HV-F022-W F/S", imageToByteArray("resource\\massager\\massager4_t.jpg"),
 				imageToByteArray("resource\\massager\\massager4_d.jpg"));
 		session.save(product4);
 
-		Product product5 = new Product("Massager", "HOT Electric Pulse Neck Massager", 770, 10,
+		ProductBean product5 = new ProductBean("Massager", "HOT Electric Pulse Neck Massager", 770, 10,
 				"Pain Relief Magnetic Therapy Vertebra Treatment",
 				imageToByteArray("resource\\massager\\massager5_t.jpg"),
 				imageToByteArray("resource\\massager\\massager5_d.jpg"));
 		session.save(product5);
 
-		Product product6 = new Product("Yoga", "高密度瑜珈墊", 179, 99, "10mm NBR環保瑜珈墊",
+		ProductBean product6 = new ProductBean("Yoga", "高密度瑜珈墊", 179, 99, "10mm NBR環保瑜珈墊",
 				imageToByteArray("resource\\yoga\\yoga1_t.jpg"), imageToByteArray("resource\\yoga\\yoga1_d.jpg"));
 		session.save(product6);
 
-		Product product7 = new Product("Yoga", "瑜珈球", 299, 99, "加厚防爆  高承重力 環保PVC",
+		ProductBean product7 = new ProductBean("Yoga", "瑜珈球", 299, 99, "加厚防爆  高承重力 環保PVC",
 				imageToByteArray("resource\\yoga\\yoga2_t.jpg"), imageToByteArray("resource\\yoga\\yoga2_d.jpg"));
 		session.save(product7);
 
-		Product product8 = new Product("Yoga", "瑜伽柱", 299, 99, "月牙型/按摩輔助/滾輪泡沫軸",
+		ProductBean product8 = new ProductBean("Yoga", "瑜伽柱", 299, 99, "月牙型/按摩輔助/滾輪泡沫軸",
 				imageToByteArray("resource\\yoga\\yoga3_t.jpg"), imageToByteArray("resource\\yoga\\yoga3_d.jpg"));
 		session.save(product8);
 
-		Product product9 = new Product("Yoga", "空中瑜伽吊床瑜珈秋千", 1487, 59, "健身吊床戶外降落傘布YOGA HAMMOCKYXS",
+		ProductBean product9 = new ProductBean("Yoga", "空中瑜伽吊床瑜珈秋千", 1487, 59, "健身吊床戶外降落傘布YOGA HAMMOCKYXS",
 				imageToByteArray("resource\\yoga\\yoga4_t.jpg"), imageToByteArray("resource\\yoga\\yoga4_d.jpg"));
 		session.save(product9);
 
-		Product product10 = new Product("Yoga", "Yoga Block", 483, 99, "LIVEUPEVA 瑜伽輔助用品家用瑜伽館工具裝備",
+		ProductBean product10 = new ProductBean("Yoga", "Yoga Block", 483, 99, "LIVEUPEVA 瑜伽輔助用品家用瑜伽館工具裝備",
 				imageToByteArray("resource\\yoga\\yoga5_t.jpg"), imageToByteArray("resource\\yoga\\yoga5_d.jpg"));
 		session.save(product10);
 
-		Product product11 = new Product("Supplement", "Rule One R1", 1495, 14, "水解乳清蛋白 天然無調味ON,BSN,MP,Myprotein",
-				imageToByteArray("resource\\supplement\\protein1_t.jpeg"),
+		ProductBean product11 = new ProductBean("Supplement", "Rule One R1", 1495, 14,
+				"水解乳清蛋白 天然無調味ON,BSN,MP,Myprotein", imageToByteArray("resource\\supplement\\protein1_t.jpeg"),
 				imageToByteArray("resource\\supplement\\protein1_d.jpeg"));
 		session.save(product11);
 
-		Product product12 = new Product("Supplement", "MusclePharm MP Combat", 1345, 30, "乳清蛋白 4磅 高蛋白乳清",
+		ProductBean product12 = new ProductBean("Supplement", "MusclePharm MP Combat", 1345, 30, "乳清蛋白 4磅 高蛋白乳清",
 				imageToByteArray("resource\\supplement\\protein2_t.jpeg"),
 				imageToByteArray("resource\\supplement\\protein2_d.jpeg"));
 		session.save(product12);
 
-		Product product13 = new Product("Supplement", "Rule One R1 Whey Blend", 3645, 24, "10磅 高蛋白乳清",
+		ProductBean product13 = new ProductBean("Supplement", "Rule One R1 Whey Blend", 3645, 24, "10磅 高蛋白乳清",
 				imageToByteArray("resource\\supplement\\protein3_t.jpeg"),
 				imageToByteArray("resource\\supplement\\protein3_d.jpeg"));
 		session.save(product13);
 
-		Product product14 = new Product("Supplement", "Optimum Nutrition ON", 1945, 99, "金牌乳清蛋白五磅 高蛋白 附防偽標籤",
+		ProductBean product14 = new ProductBean("Supplement", "Optimum Nutrition ON", 1945, 99, "金牌乳清蛋白五磅 高蛋白 附防偽標籤",
 				imageToByteArray("resource\\supplement\\protein4_t.jpeg"),
 				imageToByteArray("resource\\supplement\\protein4_d.jpeg"));
 		session.save(product14);
 
-		Product product15 = new Product("Supplement", "MYPROTEIN", 2599, 99, "正品 附發票（現貨）5公斤 2.5公斤 1公斤 原廠 高蛋白 乳清蛋白 蛋白粉",
-				imageToByteArray("resource\\supplement\\protein4_t.jpeg"),
+		ProductBean product15 = new ProductBean("Supplement", "MYPROTEIN", 2599, 99,
+				"正品 附發票（現貨）5公斤 2.5公斤 1公斤 原廠 高蛋白 乳清蛋白 蛋白粉", imageToByteArray("resource\\supplement\\protein4_t.jpeg"),
 				imageToByteArray("resource\\supplement\\protein4_d.jpeg"));
 		session.save(product15);
 
