befe1d03585c1a6e63f308274fa1c0146e9e06d2
yisheng112358 <chenyisheng112358@gmail.com>
2020-09-09 20:24:31 +0800

    update the filter for the session the singleton

--- Develop/src/tw/wematch/util/OpenSessionViewFilter.java
+++ Develop/src/tw/wematch/util/OpenSessionViewFilter.java
@@ -4,38 +4,50 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 public class OpenSessionViewFilter implements Filter {
 
-	private Session session;
+	private SessionFactory sessionFactory;
+	private WebApplicationContext context;
+
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+		String sessionFactoryBeanName = config.getInitParameter("sessionFactoryBeanName");
+		ServletContext application = config.getServletContext();
+		context = WebApplicationContextUtils.getWebApplicationContext(application);
+		sessionFactory = (SessionFactory) context.getBean(sessionFactoryBeanName);
+	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		try {
-			SessionFactory factory = HibernateUtil.getSessionFactory();
-			session = factory.getCurrentSession();
-			session.beginTransaction();
-			System.out.println("Transaction Begin.");
-
+			sessionFactory.getCurrentSession().beginTransaction();
+			System.out.println("Filter Transaction Begin.");
 			chain.doFilter(request, response);
-
-			session.getTransaction().commit();
-			System.out.println("Transaction Commit.");
+			sessionFactory.getCurrentSession().getTransaction().commit();
 		} catch (Exception e) {
-			session.getTransaction().rollback();
-			System.out.println("Transaction Rollback.");
-			e.printStackTrace();
+			sessionFactory.getCurrentSession().getTransaction().rollback();
+			System.out.println("Filter Transaction RollBack.");
+			chain.doFilter(request, response);
 		} finally {
-			System.out.println("Session Closed.");
+			System.out.println("Filter Transaction Closed.");
 		}
+	}
 
+	@Override
+	public void destroy() {
+		((ConfigurableApplicationContext) context).close();
 	}
 
 }
