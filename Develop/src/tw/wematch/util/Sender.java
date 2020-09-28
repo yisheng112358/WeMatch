4d5b714a9c3e42b9daa7dce5acf6d78d0860e7cf
yisheng112358 <chenyisheng112358@gmail.com>
2020-09-29 01:39:06 +0800

    email notice setting refinement

--- Develop/src/tw/wematch/util/Sender.java
+++ Develop/src/tw/wematch/util/Sender.java
@@ -12,78 +12,57 @@ import org.apache.commons.mail.HtmlEmail;
  */
 public class Sender extends java.lang.Thread {
 	private String content;
-
 	private org.apache.log4j.Logger log;
 	private String subject;
 	private String to;
-	private static Properties p = null;
+	private static Properties property = null;
 
 	public static void loadProperties() {
 		try {
-			p = new Properties();
-			// log.info(this.getClass().getResource("/mail.properties").getPath());
-			System.out.println(Sender.class.getResource("/mail.properties"));
+			property = new Properties();
+			System.out.println("使用設定檔路徑：" + Sender.class.getResource("/mail.properties"));
 			java.io.BufferedReader reader = new java.io.BufferedReader(
 					new java.io.FileReader(Sender.class.getResource("/mail.properties").getFile()));
-			;
-			String str = null;
-			while ((str = reader.readLine()) != null) {
-				String sp[] = str.split("=");
-				p.put(sp[0].trim(), sp[1].trim());
-				System.out.println(sp[0].trim() + "," + sp[1].trim());
+			String maillSettings = null;
+			while ((maillSettings = reader.readLine()) != null) {
+				String maillSettingKeyValue[] = maillSettings.split("=");
+				property.put(maillSettingKeyValue[0].trim(), maillSettingKeyValue[1].trim());
+				System.out.println(maillSettingKeyValue[0].trim() + ": " + maillSettingKeyValue[1].trim());
 			}
-
 			reader.close();
 		} catch (java.io.IOException e) {
-
 		}
 	}
 
 	public Sender(String to, String subject, String content) {
 		super();
-		if (p == null)
+		if (property == null)
 			loadProperties();
 		this.content = content;
 		this.to = to;
 		log = org.apache.log4j.Logger.getLogger(this.getClass());
-		// this.p = p;
 		this.subject = subject;
 	}
 
 	public void run() {
 		HtmlEmail email = new HtmlEmail();
-
 		try {
-
-			String host = p.getProperty("host");
-
-			String from = p.getProperty("from");
-			String from_name = p.getProperty("fromName");
-
-			String user = p.getProperty("user");
-			String pwd = p.getProperty("pwd");
-			String port = p.getProperty("port");
-
-			email.setTLS(true); // 是否TLS檢驗，某些email需要TLS安全檢驗，同理有SSL檢驗
-			// email.setSSL(true);
+			String host = property.getProperty("host");
+			String from = property.getProperty("from");
+			String from_name = property.getProperty("fromName");
+			String user = property.getProperty("user");
+			String pwd = property.getProperty("pwd");
+			String port = property.getProperty("port");
+			email.setTLS(true);
 			email.setHostName(host);
-			email.setAuthenticator(new DefaultAuthenticator(user, pwd)); // 使用者帳號及密碼
-			// email.setAuthentication(user, user);
-			// email.setSslSmtpPort(port);
+			email.setAuthenticator(new DefaultAuthenticator(user, pwd));
 			email.setSmtpPort(Integer.parseInt(port));
-
 			email.setFrom(from, from_name);
 			email.setCharset("utf-8");
-
-			email.addTo(to); // 接收方
-			// email.addCc("yyyyyy@gmail.com"); //副本
-			// email.addBcc("zzzzzz@gmail.com"); //密件副本
-			email.setSubject(subject); // 標題
-
-			// email.setTextMsg("Your email client does not support HTML messages");
-			email.setHtmlMsg(content); // 内容
+			email.addTo(to);
+			email.setSubject(subject);
+			email.setHtmlMsg(content);
 			email.send();
-
 		} catch (EmailException e) {
 			e.printStackTrace();
 			log.info(e);
@@ -92,9 +71,5 @@ public class Sender extends java.lang.Thread {
 			log.info(e);
 		}
 	}
-
-//	public static void main(String args[]) {
-//		(new Sender("jmtforg@gmail.com", "test", "test")).start();
-//	}
 
 }
